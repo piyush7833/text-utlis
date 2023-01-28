bedb6e34422a5572ecdaef9766296cfb6bac9d93
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-01-28 18:52:05 +0530

    state and hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/textForm';
 function App() {
   return (
     <>
- <Navbar title="Text-Utils"  about="About us"/> {/* passing props  //props are read only */} 
+ <Navbar title="Text-Utils"  about="About us"/> {/* passing props  //props are read only */}
+ <div className="container">
+ <TextForm heading="Enter your text to modify" example="Hello Piyush"/>
+ </div>
 </>
   );
 }
 
+
+//Every component have a state which changes when user interfere with that component
 export default App;
--- src/components/textForm.js
+++ src/components/textForm.js
@@ -1,1 +1,39 @@
-im
+import React from 'react'
+import {useState} from 'react'  
+
+
+export default function TextForm(props) {
+    const [text, setText] = useState("Enter text here");  //default text is Enter text here and whenever we want to acess updated state we use settext function here
+    // we can not update text directly we have to use our setText function for this i.e. text="jsdjk"; is not valid, settext("bsdgd"); is valid
+
+    // setText("New text");
+
+
+    const upperCase= ()=>{
+        console.log("Uppercase is clicked");
+        let uptext=text.toUpperCase();
+        setText(uptext);
+    }
+    const lowerCase= ()=>{
+        console.log("Uppercase is clicked");
+        let ltext=text.toLowerCase();
+        setText(ltext);
+    }
+    const handleOnChange= (event)=>{
+        // console.log("OnChange");
+        setText(event.target.value);  //if we not do this then we can not change the form value
+    }
+  return (
+    <>
+<div className="mb-3">
+  <h1 className="my-3">{props.heading}</h1>
+  <textarea className="form-control" id="myBox" rows="10" value={text} onChange={handleOnChange} placeholder={props.example}></textarea>  {/*setting text/*/}
+
+  <button className="btn btn-primary my-3 mx-3" onClick={upperCase}>Convert to Uppercase</button>
+  <button className="btn btn-primary my-3 mx-3" onClick={lowerCase}>Convert to Lowercase</button>
+</div>
+    </>
+  )
+}
+//hooks are new edition in react 16.8 which let developer use state and other react features without writing class as we are using function based component
+
