29f7fb8c152f15c40bc529742d7fd92492e541d6
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-01-28 18:02:39 +0530

    navbar

---
+++ module1.mjs
@@ -0,0 +1,5 @@
+import x,{a,c,b} from './module2.mjs' ;
+console.log(x);
+console.log(a);
+console.log(c);
+console.log(b);
---
+++ module2.mjs
@@ -0,0 +1,12 @@
+const a=32;
+const b=23;
+const c=7833;
+const d="Piyush";
+
+//default export
+export default d;
+
+//named export
+export {a};
+export {c};
+export {b};
