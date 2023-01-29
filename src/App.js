09b3a71f4d60aed87f9223c0b3b06e0bfee50049
piyush7833 <96736508+piyush7833@users.noreply.github.com>
2023-01-29 11:31:39 +0530

    dark+features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/textForm';
+// import About from './components/About';
 function App() {
+  // let darkStyle={
+  //   backgroundColour:'black',
+  //   colour:'red'
+  // }
   return (
     <>
  <Navbar title="Text-Utils"  about="About us"/> {/* passing props  //props are read only */}
  <div className="container">
- <TextForm heading="Enter your text to modify" example="Hello Piyush"/>
+ <TextForm heading="Enter your text to modify" example="Enter your text here"/>
+ {/* <About/> */}
  </div>
 </>
   );
--- src/components/textForm.js
+++ src/components/textForm.js
@@ -3,7 +3,7 @@ import {useState} from 'react'
 
 
 export default function TextForm(props) {
-    const [text, setText] = useState("Enter text here");  //default text is Enter text here and whenever we want to acess updated state we use settext function here
+    const [text, setText] = useState("");  //default text is Enter text here and whenever we want to acess updated state we use settext function here
     // we can not update text directly we have to use our setText function for this i.e. text="jsdjk"; is not valid, settext("bsdgd"); is valid
 
     // setText("New text");
@@ -14,10 +14,26 @@ export default function TextForm(props) {
         let uptext=text.toUpperCase();
         setText(uptext);
     }
+    const clear= ()=>{
+        // console.log("Lowercase is clicked");
+        let ltext="";
+        setText(ltext);
+    }
     const lowerCase= ()=>{
         // console.log("Lowercase is clicked");
         let ltext=text.toLowerCase();
         setText(ltext);
+    }
+
+    const Copy= ()=>{
+        let copy=document.getElementById('myBox');
+        copy.select();
+        // copy.setSelectionRange(0,text.length);    //not needed
+        navigator.clipboard.writeText(copy.value);
+    }
+    const extraSpace= ()=>{
+        let newText=text.split(/[ ]+/);  //wherever there is a space convert it into array and give it to newText
+        setText(newText.join(" "));     //join newtext values with a single space
     }
     const handleOnChange= (event)=>{
         // console.log("OnChange");
@@ -30,14 +46,19 @@ export default function TextForm(props) {
   <h1 className="my-3">{props.heading}</h1>
   <textarea className="form-control" id="myBox" rows="10" value={text} onChange={handleOnChange} placeholder={props.example}></textarea>  {/*setting text/*/}
 
-  <button className="btn btn-primary my-3 mx-3" onClick={upperCase}>Convert to Uppercase</button>
-  <button className="btn btn-primary my-3 mx-3" onClick={lowerCase}>Convert to Lowercase</button>
+  <button className="btn btn-primary my-3 mx-1" onClick={clear}>Clear</button>
+  <button className="btn btn-primary my-3 mx-1" onClick={upperCase}>Convert to Upper case</button>
+  <button className="btn btn-primary my-3 mx-1" onClick={lowerCase}>Convert to Lower case</button>
+  {/* <button className="btn btn-primary my-3 mx-1" onClick={capitalCase}>Convert to capitalized case</button> */}
+  <button className="btn btn-primary my-3 mx-1" onClick={extraSpace}>Remove extra paces</button>
+  <button className="btn btn-primary my-3 mx-1" onClick={Copy}>Copy</button>
 </div>
 
 <div className="container my-2">
     <h1>Your text summary </h1>
     <p>Word Count ={text.split(" ").length} </p>  {/*text.split(" ") returns array separated with gap and length counts array size*/}
-    <p>Charecter Count ={text.length} </p>      {/*text.length count charecters in text including space*/}
+    <p>Charecter Count including spaces ={text.length} </p>      {/*text.length count charecters in text including space*/}
+    <p>Charecter Count excluding space ={text.length - text.split(" ").length+1} </p>      {/*text.length count charecters in text including space*/}
     
     <p>Read time = {0.004*text.split(" ").length} Minutes read</p>
     <h2>Preview</h2>
